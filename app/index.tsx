6d4b3fa204514bec24cf033f66c0fee538265f6a
emreayhn <eayhan20@posta.pau.edu.tr>
2024-10-21 11:12:41 +0300

    States

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,86 @@
 import { Text, View, TextInput, StyleSheet, TouchableOpacity, } from 'react-native';
 import { Link } from 'expo-router';
-import React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+
+import React, { useState, useEffect } from 'react';
 import { opacity } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+import axios from 'axios';
+
+const BASE_URL = "https://opentdb.com/api.php?amount=10";
+const CATEGORY_URL = "https://opentdb.com/api_category.php";
+const QUESTİON_URL = "https://opentdb.com/api_count_global.php"
+
+
 export default function Index() {
   const [selectedDifficulty, setSelectedDifficulty] = useState("");
-  
-  
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedQuestion, setSelectedQuestion] = useState("");
+  const [question, setQuestion] = useState("");
+  const [selectedNumber, setSelectedNumber] = useState(1);
+  const [selectedType, setSelectedType] = useState([]);
+  const [name, setName] = useState("")
+
+
+
+  const type = ["Rastgele", "Doğru/Yanlış ", "Çoktan Seçmeli"];
+
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get(CATEGORY_URL);
+      setCategories(response.data.trivia_categories);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchQuestion = async () => {
+    try {
+      const response = await axios.get(QUESTİON_URL);
+      setQuestion(response.data.trivia_question);
+      console.log(response.data);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  useEffect(() => {
+
+    fetchCategories();
+
+  }, []);
+
+
+  useEffect(() => {
+
+    fetchQuestion();
+  }, []);
+
+  const handlePress = () => {
+    name
+    selectedDifficulty
+    selectedCategory
+    selectedNumber
+    selectedType
+
+  }
+
   return (// #2D7C86  #FBC02D  #BF360C
     <View style={styles.container}>
-    
-       <View style={styles.boxName}>
-          <TextInput style={styles.textInput} placeholder='Adınızı Giriniz ' placeholderTextColor="white"  >
-          </TextInput>
-          </View>
-          <View style={styles.boxContainer}>
-            <View style={styles.boxZorlukContainer} >
-              
-            <TouchableOpacity
+
+      <View style={styles.boxName}>
+        <TextInput style={styles.textInput} placeholder='Adınızı Giriniz ' placeholderTextColor="white" 
+        value={name}
+        onChangeText={setName} >
+        </TextInput>
+      </View>
+      <View style={styles.boxContainer}>
+        <View style={styles.boxZorlukContainer} >
+
+          <TouchableOpacity
             style={[styles.boxZorluk, selectedDifficulty === 'easy' ? { backgroundColor: '#B3E0E5' } : { backgroundColor: '#2D7C86' }]}
             onPress={() => setSelectedDifficulty('easy')}
           >
@@ -34,41 +98,53 @@ export default function Index() {
           >
             <Text style={styles.text}>Zor</Text>
           </TouchableOpacity>
-            </View>
-            <View style={styles.boxSelectContainer}>
-              
-            <select style={styles.select}>
-        <option value="" disabled>
-          Seçiniz
-        </option>
-        <option value="option1">Seçenek 1</option>
-        <option value="option2">Seçenek 2</option>
-        <option value="option3">Seçenek 3</option>
-      </select>
-      <select style={styles.select}>
-        <option value="" disabled>
-          Seçiniz
-        </option>
-        <option value="option1">Seçenek 1</option>
-        <option value="option2">Seçenek 2</option>
-        <option value="option3">Seçenek 3</option>
-      </select>
-      <select style={styles.select}>
-        <option value="" disabled>
-          Seçiniz
-        </option>
-        <option  style={styles.select} value="option1">Seçenek 1</option>
-        <option value="option2">Seçenek 2</option>
-        <option value="option3">Seçenek 3</option>
-      </select>
-      </View>
-          </View>
-          <TouchableOpacity style={styles.button}>
+        </View>
+        <View style={styles.boxSelectContainer}>
+          <Picker
+            selectedValue={selectedCategory}
+            style={styles.select}
+            onValueChange={(itemValue) => setSelectedCategory(itemValue)}
+          >
+            <Picker.Item label="Kategori " value="" />
+            {categories.map(category => (
+              <Picker.Item key={category.id} label={category.name} value={category.id} />
 
-<Link href="/about" style={styles.text}>
-        Başla
-      </Link>
-          </TouchableOpacity>
+            ))}
+
+          </Picker>
+
+
+          <Picker
+            selectedValue={selectedNumber}
+            style={styles.select}
+            onValueChange={(itemValue) => setSelectedNumber(itemValue)}
+          >
+
+            {[...Array(100)].map((_, index) => (
+              <Picker.Item key={index + 1} label={(index + 1).toString()} value={index + 1} />
+            ))}
+          </Picker>
+
+
+          <Picker
+        selectedValue={selectedType}
+        style={styles.select}
+        onValueChange={(itemValue) => setSelectedType(itemValue)}
+      >
+        <Picker.Item label="Tip" value="" />
+        {type.map((type, index) => (
+          <Picker.Item key={index} label={type} value={type} />
+        ))}
+      </Picker>
+          
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button} >
+
+        <Link href="/about" style={styles.text}>
+          Başla
+        </Link>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -76,98 +152,98 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#D7CCC8'
+    backgroundColor: '#D7CCC8'
   },
-  boxName:{
-   
-   // backgroundColor:'yellow',
-    alignItems:'center',
-    height:75,
-  // marginTop:'7%',
-   paddingTop:'2%',
-   
+  boxName: {
+
+    // backgroundColor:'yellow',
+    alignItems: 'center',
+    height: 75,
+    // marginTop:'7%',
+    paddingTop: '2%',
+
 
   },
   text: {
     color: 'white',
-    fontWeight:'bold',
-    fontFamily:'Roboto, sans-serif'
+    fontWeight: 'bold',
+    fontFamily: 'Roboto, sans-serif'
   },
   button: {
-    width:'15%',
-    height:'10%',
+    width: '15%',
+    height: '10%',
     fontSize: 20,
     //textDecorationLine:'line-through',
     color: 'white',
-    backgroundColor:'#969696',
-    justifyContent:'center',
-    alignItems:'center',
-    marginHorizontal:'42.5%',
-  // marginVertical:'10%',
-   borderRadius:15,
-   fontWeight:'bold',
-  
+    backgroundColor: '#969696',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: '42.5%',
+    // marginVertical:'10%',
+    borderRadius: 15,
+    fontWeight: 'bold',
+
   },
-  textInput:{
-    color:'white',
-    backgroundColor:'#969696',
-    borderRadius:20,
-    justifyContent:'center',
-    textAlign:'center',
-    height:50,
-    borderColor:'#B2B2B2',
+  textInput: {
+    color: 'white',
+    backgroundColor: '#969696',
+    borderRadius: 20,
+    justifyContent: 'center',
+    textAlign: 'center',
+    height: 50,
+    borderColor: '#B2B2B2',
     fontWeight: 'bold'
-    
+
   },
   boxContainer: {
     flexDirection: 'row',
-   justifyContent:'space-evenly',
+    justifyContent: 'space-evenly',
     //backgroundColor:'pink',
     //paddingHorizontal: 20,
-    height:'50%',
-    paddingHorizontal:'5%',
-    paddingVertical:'2%',
-    alignItems:'center',
-    marginVertical:'5%'
-    
+    height: '50%',
+    paddingHorizontal: '5%',
+    paddingVertical: '2%',
+    alignItems: 'center',
+    marginVertical: '5%'
+
   },
   boxZorlukContainer: {
-     // backgroundColor:'white',
-      width:'10%',
-      height:'80%',
-      justifyContent:'center',
-      alignItems:'center',
-      
-   
+    // backgroundColor:'white',
+    width: '10%',
+    height: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+
   },
-  boxZorluk:{
-    width:'90%',
-    height:'30%',
-   // backgroundColor:'#607D8B',
-    borderRadius:15,
-    margin:'5%',
-    justifyContent:'center',
-    alignItems:'center',
-   
+  boxZorluk: {
+    width: '90%',
+    height: '30%',
+    // backgroundColor:'#607D8B',
+    borderRadius: 15,
+    margin: '5%',
+    justifyContent: 'center',
+    alignItems: 'center',
+
   },
   boxSelectContainer: {
-    width:'50%',
-    height:'20%',
-    flexDirection:'row',
-   // backgroundColor:'pink',
+    width: '50%',
+    height: '20%',
+    flexDirection: 'row',
+    // backgroundColor:'pink',
     justifyContent: 'space-between',
-    alignItems:'center',
-   // paddingTop:'10%'
-   // width:'80%'
+    alignItems: 'center',
+    // paddingTop:'10%'
+    // width:'80%'
   },
   select: {
-    width:'30%',
-    height:'70%',
-    color:'white',
-    backgroundColor:'#BF6240',
-    borderRadius:10,
-    fontWeight:'bold',
-    padding:'1%'
+    width: '30%',
+    height: '70%',
+    color: 'white',
+    backgroundColor: '#BF6240',
+    borderRadius: 10,
+    fontWeight: 'bold',
+    padding: '1%'
   }
-  
+
 });
